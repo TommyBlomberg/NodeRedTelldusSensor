6428cb08aa2274e0d477fcad1e53655915b797c9
TommyBlomberg <Tommy.Blomberg.1@gmail.com>
2019-03-19 22:45:19 +0100

    Added Id property

--- telldus-sensor.js
+++ telldus-sensor.js
@@ -1,13 +1,13 @@
-module.exports = function (RED) {
-    function TelldusSensorNode(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
+module.exports = function(RED) {
+  function TelldusSensorNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
 
-        node.on('input', function (msg) {
-            msg.payload = "Basic skeleton, no real functionality. WIP...";
-            node.send(msg);
-        });
-    }
+    node.on("input", function(msg) {
+      msg.payload = "The Id of the node is: " + node.id;
+      node.send(msg);
+    });
+  }
 
-    RED.nodes.registerType("telldus-sensor", TelldusSensorNode);
-}
+  RED.nodes.registerType("telldus-sensor", TelldusSensorNode);
+};
