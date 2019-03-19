57dfb05a9a51a6688b281c5614b14f83bd299e32
TommyBlomberg <Tommy.Blomberg.1@gmail.com>
2019-03-19 23:21:53 +0100

    Added credentials properties

--- telldus-sensor.js
+++ telldus-sensor.js
@@ -4,10 +4,17 @@ module.exports = function(RED) {
     var node = this;
 
     node.on("input", function(msg) {
-      msg.payload = "The Id of the node is: " + node.id;
+      msg.payload = `The Id of ${node.name} is ${node.id}`;
       node.send(msg);
     });
   }
 
-  RED.nodes.registerType("telldus-sensor", TelldusSensorNode);
+  RED.nodes.registerType("telldus-sensor", TelldusSensorNode, {
+    credentials: {
+      public_key: { type: "text", required: true },
+      private_key: { type: "text", required: true },
+      token: { type: "text", required: true },
+      token_secret: { type: "text", required: true }
+    }
+  });
 };
