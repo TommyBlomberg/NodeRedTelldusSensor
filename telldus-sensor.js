79c19462fec6b182dadeb6b8d3fd586bcfa73187
TommyBlomberg <Tommy.Blomberg.1@gmail.com>
2019-03-20 23:39:34 +0100

    Improved error handling and implemented api login

--- telldus-sensor.js
+++ telldus-sensor.js
@@ -5,14 +5,47 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
     var node = this;
 
-    var api = new TelldusAPI.TelldusAPI({
-      publicKey: this.credentials.public_key,
-      privateKey: this.credentials.private_key
-    });
-
     node.on("input", function(msg) {
-      msg.payload = `The Id of ${node.name} is ${node.id}`;
-      node.send(msg);
+      var missingCredentials = [];
+      if (this.credentials.public_key == "" || this.credentials.public_key == null) {
+        missingCredentials.push("Public key");
+      }
+      if (this.credentials.private_key == "" || this.credentials.private_key == null) {
+        missingCredentials.push("Private key");
+      }
+      if (this.credentials.token == "" || this.credentials.token == null) {
+        missingCredentials.push("Token");
+      }
+      if (this.credentials.token_secret == "" || this.credentials.token_secret == null) {
+        missingCredentials.push("Token secret");
+      }
+
+      if (missingCredentials.length > 0) {
+        var errorMessage = `Error: Following credentials are missing - ${missingCredentials.join(", ")}`;
+        node.status({ fill: "red", shape: "ring", text: errorMessage });
+        node.error(errorMessage);
+        return;
+      }
+
+      var api = new TelldusAPI.TelldusAPI({
+        publicKey: this.credentials.public_key,
+        privateKey: this.credentials.private_key
+      });
+
+      api.login(this.credentials.token, this.credentials.token_secret, function(err, user) {
+        if (!!err) {
+          var errorMessage = `Login failed: ${err.message}`;
+          node.status({ fill: "red", shape: "ring", text: errorMessage });
+
+          return node.log(errorMessage);
+        }
+
+        node.status({ fill: "green", shape: "dot", text: `Login succesful for ${user.firstname} ${user.lastname}` });
+        node.log(`user: ${JSON.stringify(user, null, 2)}`);
+
+        msg.payload = `The Id of ${node.name} is ${node.id}`;
+        node.send(msg);
+      });
     });
   }
 
